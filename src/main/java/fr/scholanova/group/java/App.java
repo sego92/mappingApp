914dd339137ef7b0e7a81a405418a9f7a253f191
sego <sego.maris@gmail.com>
2018-02-13 19:03:54 +0100

    commit

--- src/main/java/fr/scholanova/group/java/App.java
+++ src/main/java/fr/scholanova/group/java/App.java
@@ -4,13 +4,11 @@ import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.convert.ConversionException;
-import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.core.convert.support.GenericConversionService;
 
 import fr.scholanova.group.java.entities.User;
-import fr.scholanova.group.java.tools.convertors.StringToUserConverter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App 
 {
 	public static void main( String[] args )
@@ -19,9 +17,9 @@ public class App
 		user.setFirstName("Javier");
 		user.setLastName("Pastore");
 
-		System.out.println(user);
+		log.debug("Print User", user);
 
-		System.out.println(System.getProperty("java.class.path"));
+		log.warn(System.getProperty("java.class.path"));
 
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("Spring/applicationContext.xml");
 
@@ -30,28 +28,29 @@ public class App
 		user3.setFirstName("Diego");
 		user3.setLastName("Simeone");
 		System.out.println(user3);
+		
+//		AspectExample ap = (AspectExample)ctx.getBean("aspectJExemple");
 
 
 		DataSource datasource = (DataSource)ctx.getBean("dataSource");
 		System.out.println(datasource);
 
 
-		GenericConversionService cs = new DefaultConversionService();
-
-		try {
-			Boolean b = cs.convert("yes", Boolean.class);
-			System.out.println(b);
-			
-			Character c = cs.convert(12, Character.class);
-			
-			
-			cs.addConverter(new StringToUserConverter());
-			
-			User user4 = cs.convert(user3.toString(), User.class);
-			
-			System.out.println(user4);
-		} catch (ConversionException e) {
-			System.out.println(e);
-		}
+//		GenericConversionService cs = new DefaultConversionService();
+//
+//		try {
+//			Boolean b = cs.convert("yes", Boolean.class);
+//			System.out.println(b);
+//			
+//			Character c = cs.convert(12, Character.class);
+//			
+//			cs.addConverter(new StringToUserConverter());
+//			
+//			User user4 = cs.convert(user3.toString(), User.class);
+//			
+//			System.out.println(user4);
+//		} catch (ConversionException e) {
+//			System.out.println(e);
+//		}
 	}
 }
--- src/main/java/fr/scholanova/group/java/tools/aspects/AspectExample.java
+++ src/main/java/fr/scholanova/group/java/tools/aspects/AspectExample.java
@@ -1,17 +1,26 @@
 package fr.scholanova.group.java.tools.aspects;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class AspectExample {
-	@Pointcut("execution(*read*(.))")
-	public void aspectjLoadTimeWeavingExample() {
-		
+	@Pointcut("execution(* read*(.))")
+	public void aspectjLoadTimeWeavingExample(){
+		// TODO : code
 	}
 	
-	@Before("execution(* setFirstName*(.))")
-	public void printTime() {
-		System.out.println(System.currentTimeMillis());
+	@Before("execution(* fr.scholanova.group.java.entities.User.setFirstName(..))")
+	public void printBefore() {
+		System.out.println("User.setFirstName aspecté avant");
 	}
-
+	
+	@After("execution(* fr.scholanova.group.java.entities.User.setFirstName(..))")
+	public void printAfter() {
+		System.out.println("User.setFirstName aspecté après");
+	}
 }
